6e573529fd3a27463832f4f74318eeda4e5b4c77
li <799478052@qq.com>
2019-06-02 13:21:15 +0800

    测试服务器端

---
+++ src/test_server/TestServer.java
@@ -0,0 +1,10 @@
+package test_server;
+
+import talk_server.TalkServer;
+
+public class TestServer {
+    public static void main(String[] args) {
+        TalkServer ts = new TalkServer();
+        ts.setVisible(true);
+    }
+}

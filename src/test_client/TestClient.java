8727b8892a40e28e102eb18e903c32b3a0e803c3
li <799478052@qq.com>
2019-06-02 13:20:48 +0800

    测试客户端

---
+++ src/test_client/TestClient.java
@@ -0,0 +1,11 @@
+package test_client;
+
+import talk_client.TalkClient;
+
+public class TestClient {
+    public static void main(String[] args) {
+        TalkClient tc1 = new TalkClient();
+        tc1.setVisible(true);
+
+    }
+}
